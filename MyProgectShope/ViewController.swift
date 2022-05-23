78c9191795a1c72fa1eadacc1fb15f4a55b86b21
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-05-23 21:20:13 +0300

    Initial Commit

---
+++ MyProgectShope/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyProgectShope
+//
+//  Created by Максим Гурков on 23.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
