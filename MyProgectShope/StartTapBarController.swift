6f8e0237cdf881538c9e759f673af8651e0a24c0
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-05-23 22:01:16 +0300

    Start

---
+++ MyProgectShope/StartTapBarController.swift
@@ -0,0 +1,29 @@
+//
+//  StartTapBarController.swift
+//  MyProgectShope
+//
+//  Created by Максим Гурков on 23.05.2022.
+//
+
+import UIKit
+
+class StartTapBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MyProgectShope/ViewController.swift
+++ MyProgectShope/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var viewInfo: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 10
+        viewInfo.layer.cornerRadius = 10
     }
 
 
