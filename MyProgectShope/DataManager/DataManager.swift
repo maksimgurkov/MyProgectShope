4322bd116bb96e1467d7297bc860e21bbe96ab8c
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-05-24 08:49:48 +0300

    DataManager and Mosel

---
+++ MyProgectShope/DataManager/DataManager.swift
@@ -0,0 +1,75 @@
+//
+//  DataManager.swift
+//  MyProgectShope
+//
+//  Created by Максим Гурков on 24.05.2022.
+//
+
+import Foundation
+
+class DataManager {
+    
+    static let shared = DataManager()
+    
+    let doors = [
+        Door(factory: .luxor,
+             material: .ecoShpone,
+             nameDoor: "Lu-21",
+             description: "Описание"),
+        Door(factory: .luxor,
+             material: .ecoShpone,
+             nameDoor: "Lu-21",
+             description: "Описание"),
+        Door(factory: .luxor,
+             material: .ecoShpone,
+             nameDoor: "Lu-21",
+             description: "Описание"),
+        Door(factory: .luxor,
+             material: .ecoShpone,
+             nameDoor: "Lu-21",
+             description: "Описание"),
+        Door(factory: .luxor,
+             material: .ecoShpone,
+             nameDoor: "Lu-21",
+             description: "Описание"),
+        Door(factory: .onyx,
+             material: .ecoShpone,
+             nameDoor: "Lu-21",
+             description: "Описание"),
+        Door(factory: .onyx,
+             material: .ecoShpone,
+             nameDoor: "Lu-21",
+             description: "Описание"),
+        Door(factory: .onyx,
+             material: .ecoShpone,
+             nameDoor: "Lu-21",
+             description: "Описание"),
+        Door(factory: .onyx,
+             material: .ecoShpone,
+             nameDoor: "Lu-21",
+             description: "Описание"),
+        Door(factory: .sodrugestvo,
+             material: .ecoShpone,
+             nameDoor: "Lu-21",
+             description: "Описание"),
+        Door(factory: .sodrugestvo,
+             material: .ecoShpone,
+             nameDoor: "Lu-21",
+             description: "Описание"),
+        Door(factory: .sodrugestvo,
+             material: .ecoShpone,
+             nameDoor: "Lu-21",
+             description: "Описание"),
+        Door(factory: .sodrugestvo,
+             material: .ecoShpone,
+             nameDoor: "Lu-21",
+             description: "Описание"),
+        Door(factory: .sodrugestvo,
+             material: .ecoShpone,
+             nameDoor: "Lu-21",
+             description: "Описание"),
+    ]
+    
+    private init() {}
+    
+}
---
+++ MyProgectShope/Model/Model.swift
@@ -0,0 +1,36 @@
+//
+//  Model.swift
+//  MyProgectShope
+//
+//  Created by Максим Гурков on 24.05.2022.
+//
+
+import Foundation
+
+
+struct Door {
+    let factory: Factory
+    let material: Material
+    let nameDoor: String
+    let description: String
+    var flag = false
+    
+    static func forDoors() -> [Door] {
+        let door = DataManager.shared.doors
+        return door
+    }
+    
+    
+}
+
+enum Material: String {
+    case shpone = "Шпон"
+    case ecoShpone = "ЭкоШпон"
+    case emal = "Эмаль"
+}
+
+enum Factory: String {
+    case luxor = "Luxor"
+    case onyx = "Оникс"
+    case sodrugestvo = "Содружество"
+}
